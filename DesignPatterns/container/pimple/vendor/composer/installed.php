a4de985164cf18dd4b1e7a7e31fe64b8fc7580a2
zhaoys <18761152733@163.com>
2021-10-21 18:14:32 +0800

    add pimple container

---
+++ DesignPatterns/container/pimple/vendor/composer/installed.php
@@ -0,0 +1,42 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'b5fb35b1b228e967e7801d53a0e158f900af99fd',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'b5fb35b1b228e967e7801d53a0e158f900af99fd',
+    ),
+    'pimple/pimple' => 
+    array (
+      'pretty_version' => 'v3.2.3',
+      'version' => '3.2.3.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '9e403941ef9d65d20cba7d54e29fe906db42cf32',
+    ),
+    'psr/container' => 
+    array (
+      'pretty_version' => '1.0.0',
+      'version' => '1.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'b7ce3b176482dbbc1245ebf52b181af44c2cf55f',
+    ),
+  ),
+);
